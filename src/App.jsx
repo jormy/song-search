55151faf1d18a0aeffdba05891556ffe3516c36e
jorm <charkuayteow.10@gmail.com>
2024-07-22 19:01:11 +0800

    basic search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import Axios from 'axios'; 
+import { useState } from 'react'; 
 
 function App() {
+  const [artist, setArtist] = useState(""); 
+  const [song, setSong] = useState(""); 
+  const [lyrics, setLyrics] = useState(""); 
 
-  return (
-    <>
-      <p className="text-5xl">
-        search
-      </p>
-    </>
+  function searchLyrics() { 
+      if (artist === "" || song === "") { 
+          return; 
+      } 
+      Axios.get( 
+`https://api.lyrics.ovh/v1/${artist}/${song}`).then(res => { 
+          console.log(res.data.lyrics); 
+          setLyrics(res.data.lyrics); 
+      }) 
+  } 
+
+  return ( 
+      <div className="App"> 
+          <h1>Lyrics Finder :3</h1> 
+
+          <input className="inp" type="text" 
+              placeholder='Artist name'
+              onChange={(e) => { setArtist(e.target.value) }} /> 
+          <input className="inp" type="text" 
+              placeholder='Song name'
+              onChange={(e) => { setSong(e.target.value) }} /> 
+          <button className="btn" 
+              onClick={() => searchLyrics()}> 
+                  Search</button> 
+          <hr /> 
+          <pre>{lyrics}</pre> 
+      </div> 
   )
 }
 
